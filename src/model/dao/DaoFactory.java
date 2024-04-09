28d087ece15ffd4721a5578db97def0f613d5fa4
gustavo oliveira <gugahb_10@hotmail.com>
2024-04-09 11:07:56 -0300

    findByAll implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.sellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createSellerDao () {
-		return new sellerDaoJDBC();
+		return new sellerDaoJDBC(DB.getConnection());
 	}
 	
 }
